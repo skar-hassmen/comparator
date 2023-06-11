bdf6fd12b2f52bf0660cae28b81cb3acb1e26e1d
skar-hassmen <94719450+skar-hassmen@users.noreply.github.com>
2023-06-11 22:56:24 +0300

    [fix] added commas

--- comparator/analizator/constants.py
+++ comparator/analizator/constants.py
@@ -1,11 +1,11 @@
 fieldnames: list = [
     "lib_name",
-    "lib_type_db"
-    "lib_version_db"
-    "func_name_db"
+    "lib_type_db",
+    "lib_version_db",
+    "func_name_db",
     "func_offset_db",
     "test_name",
-    "test_type"
+    "test_type",
     "func_name_test",
     "func_offset_test",
     "similarity"
