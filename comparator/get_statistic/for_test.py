8dfa595caa99694fcc28176748176765500331fb
skar-hassmen <94719450+skar-hassmen@users.noreply.github.com>
2023-06-12 22:40:28 +0300

    Revert "Revert "[feature] search dll/lib""
    This reverts commit 14ee397f9f7abb273c2128902b862b25eba257bf.

--- comparator/get_statistic/for_test.py
+++ comparator/get_statistic/for_test.py
@@ -1,95 +1,97 @@
+import os
+
 from cfg_utils import *
 from constants import list_instr
 from StatisticDataBase import StatisticDataBase
-
-import os
+import config_db
 
 import idaapi
-import idc 
+import idc
 import idautils
 
 
 def get_statistic_instructions(func) -> dict:
-	instructions: list = [instr_ea for instr_ea in idautils.Heads(func.start_ea, func.end_ea)]
-	statistic_instructions: dict = dict()
-	count_bytes: int = 0
+    instructions: list = [instr_ea for instr_ea in idautils.Heads(func.start_ea, func.end_ea)]
+    statistic_instructions: dict = dict()
+    count_bytes: int = 0
 
-	for instr_name in list_instr:
-		statistic_instructions[instr_name] = 0
+    for instr_name in list_instr:
+        statistic_instructions[instr_name] = 0
 
-	sum_id: int = 0
-	for instr_ea in instructions:
-		count_bytes += idc.get_item_size(instr_ea)
-		instr_name: str = ida_ua.ua_mnem(instr_ea)
+    sum_id: int = 0
+    for instr_ea in instructions:
+        count_bytes += idc.get_item_size(instr_ea)
+        instr_name: str = ida_ua.ua_mnem(instr_ea)
 
-		if instr_name in list_instr:
-			statistic_instructions[instr_name] += 1
+        if instr_name in list_instr:
+            statistic_instructions[instr_name] += 1
 
-		insn = idaapi.insn_t()
-		if idaapi.decode_insn(insn, instr_ea) > 0:
-			# сумма идентификаторов опкодов
-			sum_id += insn.itype	
+        insn = idaapi.insn_t()
+        if idaapi.decode_insn(insn, instr_ea) > 0:
+            # сумма идентификаторов опкодов
+            sum_id += insn.itype
 
-	statistic_instructions["count_bytes"] = count_bytes
-	statistic_instructions["sum_identify"] = sum_id
+    statistic_instructions["count_bytes"] = count_bytes
+    statistic_instructions["sum_identify"] = sum_id
 
-	return statistic_instructions
+    return statistic_instructions
 
 
 def serializer_instructions(statistic_instructions: dict) -> list[tuple]:
-	list_instructions: list[tuple] = list()
+    list_instructions: list[tuple] = list()
 
-	for key, value in statistic_instructions.items():
-		if key != "count_bytes" and key != "sum_identify":
-			list_instructions.append(tuple([key, value]))
+    for key, value in statistic_instructions.items():
+        if key != "count_bytes" and key != "sum_identify":
+            list_instructions.append(tuple([key, value]))
 
-	return list_instructions
+    return list_instructions
 
 
 def get_info_file() -> tuple:
-	input_file: str = get_input_file_path().split("\\")[-1]
-	tokens: list = input_file.split('.')
+    input_file: str = get_input_file_path().split("\\")[-1]
+    tokens: list = input_file.split('.')
 
-	return tokens[0], tokens[-1]
+    return tokens[0], tokens[-1]
 
 
 def entry_point():
-	func_list: list = [func for func in idautils.Functions()]
-	
-	info_about_file: tuple = get_info_file()
+    func_list: list = [func for func in idautils.Functions()]
 
-	db = StatisticDataBase()
-	db.start_connection()
+    info_about_file: tuple = get_info_file()
 
-	is_test_file: bool = True
+    db = StatisticDataBase(config_db.host, config_db.user, config_db.password, config_db.db_name)
+    db.start_connection()
 
-	id_lib: int = db.insert_data_for_lib(info_about_file[0], info_about_file[1], 1, is_test_file)
+    is_test_file: bool = True
 
-	for func_entry in func_list:
-		func = ida_funcs.get_func(func_entry)
-		statistic_instructions: dict = get_statistic_instructions(func)
+    id_lib: int = db.insert_data_for_lib(info_about_file[0], info_about_file[1], 1, is_test_file)
 
-		if statistic_instructions["count_bytes"] <= 10:
-			continue
+    for func_entry in func_list:
+        func = ida_funcs.get_func(func_entry)
+        statistic_instructions: dict = get_statistic_instructions(func)
 
-		name_function = idc.get_func_name(func_entry)
+        if statistic_instructions["count_bytes"] <= 15:
+            continue
 
-		func_nodes: list = getNodes(func)
-		func_edges: list = getEdges(func_nodes)
-		
-		graph: str = ""
-		for edge in func_edges:
-			graph += f'{edge};'
+        name_function = idc.get_func_name(func_entry)
 
-		id_function: int = db.insert_data_for_function(name_function, hex(func_entry), statistic_instructions["count_bytes"], statistic_instructions["sum_identify"], graph, id_lib)
-		db.insert_data_for_instruction(serializer_instructions(statistic_instructions), id_function)
+        func_nodes: list = getNodes(func)
+        func_edges: list = getEdges(func_nodes)
 
-	db.close_connection()
+        graph: str = ""
+        for edge in func_edges:
+            graph += f'{edge};'
+
+        id_function: int = db.insert_data_for_function(name_function, hex(func_entry),
+                                                       statistic_instructions["count_bytes"],
+                                                       statistic_instructions["sum_identify"], graph, id_lib)
+        db.insert_data_for_instruction(serializer_instructions(statistic_instructions), id_function)
+
+    db.close_connection()
 
 
 if __name__ == '__main__':
-	entry_point()
+    entry_point()
 
 if "DO_EXIT" in os.environ:
-	idc.qexit(1)
-
+    idc.qexit(1)
--- comparator/main.py
+++ comparator/main.py
@@ -2,8 +2,25 @@ from get_statistic.StatisticDataBase import StatisticDataBase
 from get_statistic import config_db
 from paths import ida_hunt, compiling_database_files, test_file, script1, script2
 from analizator import analizator
+from constants import fieldnames
+from rw_csv import write_data_to_csv_file
+
+from search import search_selen
+from search import search_git
 
 import os
+
+
+def serializer_data_for_csv(rows: list) -> list:
+    result: list = list()
+
+    for row in rows:
+        temp: dict = dict()
+        for i in range(0, len(fieldnames)):
+            temp[fieldnames[i]] = row[i + 1]
+        result.append(temp)
+
+    return result
 
 
 def menu():
@@ -14,45 +31,72 @@ def menu():
             2. analize test file
             3. create database
             4. delete database
-            5. exit program
+            5. write data result to csv
+            6. search DuckDuckGo
+            7. search GitHub
+            8. exit program
         '''
     )
 
 
 def choose_operation(db):
-    state_database: bool = db.check_table()
+    state_database: bool = db.check_table("libs")
 
     while True:
         menu()
-        command: int = int(input("Input command: "))
+
+        try:
+            command: int = int(input("Input command: "))
+        except ValueError:
+            print("[INFO] Wrong command!")
+            continue
+
         if command == 1:
             if state_database is True:
                 os.system(
-                    f'python.exe {ida_hunt} --inputdir {compiling_database_files} --analyse --filter "filters\\names.py -a 32 -v" --scripts {script1}'
+                    f'python.exe {ida_hunt} --inputdir {compiling_database_files} --analyse --filter "filters\\names.py -a 64 -v" --scripts {script1}'
                 )
             else:
-                print("[INFO] Database is already empty")
+                print("[INFO] database is already empty")
         elif command == 2:
             if state_database is True:
                 os.system(
-                    f'python.exe {ida_hunt} --inputdir {test_file} --analyse --filter "filters\\names.py -a 32 -v" --scripts {script2}'
+                    f'python.exe {ida_hunt} --inputdir {test_file} --analyse --filter "filters\\names.py -a 64 -v" --scripts {script2}'
                 )
+
+                if db.check_table("results") is True:
+                    db.delete_results_table()
+
+                db.create_results_table()
                 analizator.start_analize(db)
             else:
-                print("[INFO] Database is already empty")
+                print("[INFO] database is already empty")
         elif command == 3:
             if state_database is False:
                 db.create_tables()
                 state_database = True
             else:
-                print("[INFO] Database already created")
+                print("[INFO] database already created")
         elif command == 4:
             if state_database is True:
                 db.delete_tables()
                 state_database = False
             else:
-                print("[INFO] Database is already empty")
+                print("[INFO] database is already empty")
         elif command == 5:
+            if db.check_table("results") is True:
+                rows: list = db.select_results()
+                write_data_to_csv_file(path="result.csv", rows=serializer_data_for_csv(rows), fieldnames=fieldnames)
+                print("[INFO] Data recording was successful")
+            else:
+                print("[INFO] Table of results is empty")
+        elif command == 6:
+            name_dll: str = input("Input name dll/lib (example func.dll or func): ")
+            search_selen.entry_point(name_dll)
+        elif command == 7:
+            name_dll: str = input("Input name dll/lib (example func.dll or func): ")
+            search_git.entry_point(name_dll)
+        elif command == 8:
             return
         else:
             print("[INFO] Wrong command!")
